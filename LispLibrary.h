315fdb6c5bd6407495dab9d39f22fb772640c0da
heow <heow@alphageeksinc.com>
2024-01-12 14:20:23 -0500

    added funciton sate

--- LispLibrary.h
+++ LispLibrary.h
@@ -1,28 +1,7 @@
-
 const char LispLibrary[] PROGMEM = R"lisplibrary(
 
-;;
-;; Running Common Lisp?
-;;   Works great on SBCL, others not so much due to lack of millisecond support
-;; 
-;;   first install QuickLisp
-;;     https://lispcookbook.github.io/cl-cookbook/getting-started.html#install-quicklisp
-;;   then load the Local Time Library
-;;     (ql:quickload :local-time)
-;;   then uncomment this:
-;;
-;;(defun millis ()
-;;  (let ((tm (local-time:now))) 
-;;    (+ (* 1000 (local-time:timestamp-to-unix tm))) ))
+;; comments confuse uLisp, see README.md
 
-;;
-;; Given a single-argument-function, a millisecond-count and initial-state value
-;; it returns a function that is scheduled to run at every millisecond-count.
-;;
-;; The returned function expects the ongoing millisecond count.  If fired, it
-;; returns the new state, otherwise the old state is stored for the next
-;; iteration and also retruned.
-;;
 (defun wrap-statetime (number-of-millis fn initial-state)
   (let ((next-time 0)
         (state     initial-state)) 
@@ -33,27 +12,12 @@ const char LispLibrary[] PROGMEM = R"lisplibrary(
             (setq state (funcall fn state)) ))
       state)))
 
-;;
-;; Converts a list to an array the hard way, a complete Common Lisp
-;; implementation includes 'coerce'
-;;
 (defun list-to-array (l)
   (let ((a (make-array (length l))))
     (dotimes (i (length l))
       (setf (aref a i) (nth i l)))
     a))
 
-;;
-;; Expects a list of millisecond time, function and initial state like this:
-;;
-;;                                          millis   lambda-fn     initial state
-;;   (defvar time-fn-state-list (list (list 500      my-do-this1   10000)
-;;                                    (list 1000     my-do-this2   20000)
-;;                                    (list 7000     my-do-this3   30000)))
-;;
-;; relies on 'wrap-statetime' to execute after the millis have expired and
-;; passes in the new computed state
-;;
 (defun run-event-loop (time-fn-state-list)
   (let* ((len         (length time-fn-state-list))
          (fn-array    (list-to-array (mapcar (lambda (fn-t)
--- README.md
+++ README.md
@@ -1,7 +1,23 @@
 
-# uLisp Managed State Event Loop
+# Managed State Event Loop Micro-Framework
+## for uLisp and Common Lisp
 
-![Managed State Event Loop as Envisioned by AI](docs/managed-state-event-loop.jpg?raw=true "As Envisioned by AI")
+![Managed State Event Loop as Envisioned by AI](docs/managed-state-event-loop.jpg?raw=true)(as envisioned by AI)
 
-* foo
-* bar
+
+With such a small codebase this is less of a framework than it is a development 
+strategy.  However, details matter so here we go...
+
+* _Have you ever needed to do multiple things a once ...or appear to?_
+
+* _Do you need to execute code while reading inputs?_
+
+* _Want to avoid messy, dangerous and expensive global state?_
+
+This is for you, a managed-state event loop.  All you do is worry about what 
+to do based on the last thing you did.  Everything else is managed, **you
+just write pure stateless functions**!
+
+![managed function state](docs/function-state.svg?raw=true)
+
+
